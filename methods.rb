7361987f1afb5ba2d5b17884286094ebc4ae2765
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-14 19:12:02 -0400

    commit tree c90e8ac7dd909bfb6c2929415ef2642de41758c6

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add (int1, int2)
+    int1 + int2
+end
+
+def halve int
+    if int.class != Integer
+        return nil
+    end
+    int / 2
+end
